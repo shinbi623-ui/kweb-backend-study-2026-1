2272f5ce28c267f0a0300faa15fdba0404153a4a
shinbi623-ui <shinbi623@korea.ac.kr>
2026-04-10 10:20:18 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.get("/bye", (req, res) => {
+    res.send("Goodbye, world!");
+});
+app.listen(8080, () => {
+    console.log('Server listening on port 8080!')
+});
+
